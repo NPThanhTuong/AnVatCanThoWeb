cc344ad9a065e813cc24dd502d2bab459eb605c2
weebNeedWeed <rivenmle@gmail.com>
2024-04-19 06:00:57 +0700

    added filtering by ward and district

--- AnVatCanThoWeb/Areas/Admin/Controllers/SnackbarsController.cs
+++ AnVatCanThoWeb/Areas/Admin/Controllers/SnackbarsController.cs
@@ -1,12 +1,15 @@
 using AnVatCanTho.DataAccess.Data;
 using AnVatCanThoWeb.Areas.Admin.Common;
 using AnVatCanThoWeb.Areas.Admin.ViewModels.Snackbars;
+using AnVatCanThoWeb.Common.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace AnVatCanThoWeb.Areas.Admin.Controllers;
 
 [Area(AdminAreaName.Value)]
+[Authorize(AuthenticationSchemes = ApplicationAuthenticationScheme.AdminScheme)]
 public class SnackbarsController : Controller
 {
     private readonly ApplicationDbContext _dbContext;
@@ -23,19 +26,45 @@ public class SnackbarsController : Controller
         var requestPageIndex = request.PageIndex; 
         var requestKeyword = request.Keyword; 
         var requestCurrentKeyword = request.CurrentKeyword;
+        
+        var requestDistrictName = request.DistrictName;
+        var requestWardName = request.WardName;
+        var requestCurrentDistrictName = request.CurrentDistrictName;
+        var requestCurrentWardName = request.CurrentWardName;
 
-        if (requestKeyword is not null)
+        if (requestKeyword is not null 
+            || requestDistrictName is not null 
+            || requestWardName is not null)
         {
             requestPageIndex = 1;
         }
-        else
-        {
-            requestKeyword = requestCurrentKeyword;
-        }
 
+        requestKeyword ??= requestCurrentKeyword;
+        requestDistrictName ??= requestCurrentDistrictName;
+        requestWardName ??= requestCurrentWardName;
+        
         ViewData["CurrentKeyword"] = requestKeyword;
+        ViewData["CurrentWardName"] = requestWardName;
+        ViewData["CurrentDistrictName"] = requestDistrictName;
 
-        var snackbars = _dbContext.SnackBars.AsQueryable();
+        var snackbars = _dbContext.SnackBars
+            .Include(x => x.Addresses)
+            .AsQueryable();
+
+        if (!string.IsNullOrEmpty(requestDistrictName))
+        {
+            snackbars = snackbars
+                .Where(x => x.Addresses
+                    .Any(y => y.DistrictName.Equals(requestDistrictName)));
+        }
+        
+        if (!string.IsNullOrEmpty(requestWardName))
+        {
+            snackbars = snackbars
+                .Where(x => x.Addresses
+                    .Any(y => y.WardName.Equals(requestWardName)));
+        }
+        
         if (!string.IsNullOrEmpty(requestKeyword))
         {
             snackbars = snackbars
@@ -43,10 +72,13 @@ public class SnackbarsController : Controller
                     || x.Description.Contains(requestKeyword)
                     || x.DisplayName.Contains(requestKeyword)
                     || x.Tel.Contains(requestKeyword)
-                    || x.Username.Contains(requestKeyword));
+                    || x.Username.Contains(requestKeyword)
+                    || x.Addresses
+                        .Any(y => y.NoAndStreet.Contains(requestKeyword)));
         }
 
-        var totalPages = await snackbars.CountAsync();
+        var totalRows = await snackbars.CountAsync();
+        var totalPages = (int)Math.Ceiling((float) totalRows / pageSize);
 
         var snackbarVms = await snackbars.Skip((requestPageIndex - 1) * pageSize)
             .Take(pageSize)
@@ -69,5 +101,26 @@ public class SnackbarsController : Controller
         };
 
         return View(response);
+    }
+
+    [HttpGet("/[area]/api/districts")]
+    public async Task<IActionResult> GetAllDistricts()
+    {
+        var districts = await _dbContext.Districts.ToListAsync();
+        return Ok(districts);
+    }
+    
+    [HttpGet("/[area]/api/wards")]
+    public async Task<IActionResult> GetAllWards([FromQuery] string? districtName)
+    {
+        var wardQueryable = _dbContext.Wards.AsQueryable();
+        if (!string.IsNullOrEmpty(districtName))
+        {
+            wardQueryable = wardQueryable.Where(x => x.DistrictName.Equals(districtName));
+        }
+        
+        var wards = await wardQueryable.ToListAsync();
+        
+        return Ok(wards);
     }
 }
--- AnVatCanThoWeb/Areas/Admin/ViewModels/Snackbars/GetAllSnackbarsRequest.cs
+++ AnVatCanThoWeb/Areas/Admin/ViewModels/Snackbars/GetAllSnackbarsRequest.cs
@@ -7,4 +7,12 @@ public class GetAllSnackbarsRequest
     public string? CurrentKeyword { get; init; }
     
     public string? Keyword { get; init; }
+
+    public string? DistrictName { get; init; }
+    
+    public string? CurrentDistrictName { get; init; }
+    
+    public string? WardName { get; init; }
+    
+    public string? CurrentWardName { get; init; }
 }
