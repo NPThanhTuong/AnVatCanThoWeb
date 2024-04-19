9275c6063d2d8d8dd80faf22f36ade61dc6b1088
NPThanhTuong <npttuong@gmail.com>
2024-04-19 15:44:55 +0700

    add real-time comment feature

---
+++ AnVatCanTho.DataAccess/Migrations/20240419025557_addCreatedAtFieldForCommentTable.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnVatCanTho.DataAccess.Migrations
+{
+    public partial class addCreatedAtFieldForCommentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Avatar",
+                table: "Snack_Bars",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                defaultValue: "no-avatar.jpg",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Star",
+                table: "Ratings",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Avatar",
+                table: "Customers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                defaultValue: "no-avatar.jpg",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LikeQuantity",
+                table: "Comments",
+                type: "int",
+                nullable: true,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Comments",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "getdate()");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Avatar",
+                table: "Administrators",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                defaultValue: "no-avatar.jpg",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Administrators",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DOB", "Password" },
+                values: new object[] { new DateTime(2024, 4, 19, 9, 55, 56, 41, DateTimeKind.Local).AddTicks(3881), "$2a$11$1piWTecWlRdRoyqDV.9s6.9CBLz0.BhHhTZM32NpzoUAa6KgvUWeK" });
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DOB", "Password" },
+                values: new object[] { new DateTime(2024, 4, 19, 9, 55, 55, 886, DateTimeKind.Local).AddTicks(6842), "$2a$11$mZ7lTjj1rKXLiylWysvY/e0nXLvQdqMa6RUlgo7YE/U7sdF5hY10." });
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DOB", "Password" },
+                values: new object[] { new DateTime(2024, 4, 19, 9, 55, 56, 41, DateTimeKind.Local).AddTicks(3199), "$2a$11$LiQdX3yep8I7tCXDEsauoukYr8QjF5vFP9YgePrjdGlFYpARuVs1." });
+
+            migrationBuilder.UpdateData(
+                table: "Snack_Bars",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DOB", "Password" },
+                values: new object[] { new DateTime(2024, 4, 19, 9, 55, 56, 344, DateTimeKind.Local).AddTicks(780), "$2a$11$WlXFQI6XzOhNAeN2bs3i6uc6aB8CsC4C8VI3c0sR/Jc/SGVEf0FBW" });
+
+            migrationBuilder.UpdateData(
+                table: "Snack_Bars",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DOB", "Password" },
+                values: new object[] { new DateTime(2024, 4, 19, 9, 55, 56, 491, DateTimeKind.Local).AddTicks(3310), "$2a$11$EB42Qhd2fPkUTUY/jpDJ8OnDoI5pa5eZ3JXr38.wpViIwCJifLDYu" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Avatar",
+                table: "Snack_Bars",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true,
+                oldDefaultValue: "no-avatar.jpg");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Star",
+                table: "Ratings",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldDefaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Avatar",
+                table: "Customers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true,
+                oldDefaultValue: "no-avatar.jpg");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LikeQuantity",
+                table: "Comments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true,
+                oldDefaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Avatar",
+                table: "Administrators",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true,
+                oldDefaultValue: "no-avatar.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Administrators",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DOB", "Password" },
+                values: new object[] { new DateTime(2024, 4, 3, 19, 55, 39, 271, DateTimeKind.Local).AddTicks(2567), "$2a$11$5TW40BYwYyBh1WFv157uXu8UsimlUlpzeCcLuT5UDtz50lLtBi8jW" });
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DOB", "Password" },
+                values: new object[] { new DateTime(2024, 4, 3, 19, 55, 39, 128, DateTimeKind.Local).AddTicks(5312), "$2a$11$JPOHzRVNtb1SHnCCje9q9.cJF0iC4Dhp3LkVslth7Ndzv9nChoPli" });
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DOB", "Password" },
+                values: new object[] { new DateTime(2024, 4, 3, 19, 55, 39, 271, DateTimeKind.Local).AddTicks(2135), "$2a$11$32PpsqyoZavoO1ziHjQeGePy2GObWuyxPkjwpkaROe12b/yg7G.42" });
+
+            migrationBuilder.UpdateData(
+                table: "Snack_Bars",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DOB", "Password" },
+                values: new object[] { new DateTime(2024, 4, 3, 19, 55, 39, 586, DateTimeKind.Local).AddTicks(215), "$2a$11$v4fl.EFctO0bciAxkDeFIOOCOAYodqb6YzYthDg6/SGIUa34GFGQi" });
+
+            migrationBuilder.UpdateData(
+                table: "Snack_Bars",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DOB", "Password" },
+                values: new object[] { new DateTime(2024, 4, 3, 19, 55, 39, 726, DateTimeKind.Local).AddTicks(846), "$2a$11$iEaqDWDD3pfc6aVF.HbgRexoRI/kW5UQ12C3YotrLUMz6OE6NOKfq" });
+        }
+    }
+}
---
+++ AnVatCanThoWeb/wwwroot/js/detail-product.js
@@ -0,0 +1,111 @@
+﻿// Pusher
+$(document).ready(function () {
+    var pusher = new Pusher('a6a81aa4ce5b84db0557', {
+        cluster: 'ap1',
+        encrypted: true
+    })
+    var channel = pusher.subscribe('comments_channel');
+
+    channel.bind('push_comment_event', function (data) {
+
+        const commentList = $('#comment-list');
+        const html = `
+        <div class="d-flex align-items-start gap-3 mt-4">
+            <img src="/images/User/${data.CustomerAvatar}" class="comment_avatar-user" alt="avatar user" />
+            <div>
+                <div class="d-flex align-items-center">
+                    <h5 class="fs-6 fw-bold mb-0 me-2">${data.CustomerDisplayName}</h5>
+                    <span class="fs-6 fw-light text-muted">${(new Date()).toLocaleString()}</span>
+                </div>
+                <p class="my-1">
+                    ${data.comment.Content}
+                </p>
+
+                <button class="comment-like-btn text-dark-gray">
+                    <i class="fas fa-thumbs-up"></i>
+                    <span class="ms-1">Thích</span>
+                </button>
+            </div>
+        </div>`;
+
+        commentList.prepend(html);
+    });
+});
+
+const commentFrm = $('#comment-form').on('submit', function (e) {
+    e.preventDefault();
+    const ProductId = parseInt($('input[name="ProductId"]').val());
+    const SnackBarId = parseInt($('input[name="SnackBarId"]').val());
+    const CustomerId = parseInt($('input[name="CustomerId"]').val());
+    const CustomerDisplayName = $('input[name="CustomerDisplayName"]').val();
+    const CustomerAvatar = $('input[name="CustomerAvatar"]').val();
+    const Content = $('textarea[name="Content"]').val();
+
+    $.ajax({
+        url: "/product/comment",
+        method: "POST",
+        data: {
+            ProductId,
+            SnackBarId,
+            CustomerId,
+            Content,
+            CustomerDisplayName,
+            CustomerAvatar
+        }
+    }).done(function (data) {
+        $('textarea[name="Content"]').val('');
+    });
+});
+
+function renderComment(comments) {
+    const commentList = $('#comment-list');
+
+    const htmls = comments.map(function (comment) {
+        return `
+        <div class="d-flex align-items-start gap-3 mt-4">
+            <img src="/images/User/${comment.avatar}" class="comment_avatar-user" alt="avatar user" />
+            <div>
+                <div class="d-flex align-items-center">
+                    <h5 class="fs-6 fw-bold mb-0 me-2">${comment.displayName}</h5>
+                    <span class="fs-6 fw-light text-muted">${(new Date(comment.createdAt)).toLocaleString()}</span>
+                </div>
+                <p class="my-1">
+                    ${comment.content}
+                </p>
+            </div>
+        </div>`;
+    });
+
+    commentList.html(htmls.join(" "));
+}
+
+$('#sortCommentSelect').on('change', HandleSortProduct);
+
+function HandleSortProduct(event) {
+    const url = new URL(window.location.href);
+    url.searchParams.set('sort', event.target.value);
+    url.searchParams.set('page', 1);
+    url.hash = 'comment-list'
+    window.location = url;
+}
+
+function NextPage(totalPage) {
+    const url = new URL(window.location.href);
+    const curPage = parseInt(url.searchParams.get("page") || 1);
+
+    if (curPage < totalPage) {
+        url.searchParams.set('page', curPage + 1);
+        url.hash = 'comment-list'
+        window.location = url;
+    }
+}
+
+function PrevPage(totalPage) {
+    const url = new URL(window.location.href);
+    const curPage = parseInt(url.searchParams.get("page") || 1);
+    if (curPage > 1) {
+        url.searchParams.set('page', curPage - 1);
+        url.hash = 'comment-list'
+        window.location = url;
+    }
+}
