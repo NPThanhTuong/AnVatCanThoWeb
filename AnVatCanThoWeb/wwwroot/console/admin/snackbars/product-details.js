e4204afc74f1c253c3167f273672dc04beafed55
weebNeedWeed <rivenmle@gmail.com>
2024-04-28 17:13:13 +0700

    added product listing feature

---
+++ AnVatCanThoWeb/wwwroot/console/admin/snackbars/product-details.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+    if(errorMessage) {
+        Swal.fire({
+            icon: "error",
+            title: "Lỗi",
+            text: errorMessage,
+        });
+    }
+});
+
+function deleteProduct(event, form) {
+    event.preventDefault();
+    Swal.fire({
+        title: "Bạn chắc chắn muốn xoá ?",
+        showDenyButton: true,
+        confirmButtonText: "Xoá",
+        denyButtonText: "Huỷ"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            form.submit();
+        }
+    });
+}
