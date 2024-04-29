3f870245b4e240130b116cc52b8e1df163bba543
Guavaaa <165045201+Guava0111@users.noreply.github.com>
2024-04-29 14:24:24 +0700

    demo logout & signup for snackbar

---
+++ AnVatCanThoWeb/Areas/SnackBar/ViewModels/Authentication/CreateAccountViewModel.cs
@@ -0,0 +1,36 @@
+﻿using AnVatCanTho.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnVatCanThoWeb.Areas.SnackBar.ViewModels.Authentication;
+
+public class CreateAccountViewModel
+{
+    //SNACKBAR
+    [DisplayName("Tên đăng nhập")]
+    public string? Username { get; set; }
+
+    [DisplayName("Số điện thoại")]
+    public string? Tel { get; set; }
+
+    [DisplayName("Email")]
+    public string? Email { get; set; }
+
+    [DisplayName("Mật khẩu")]
+    [DataType(DataType.Password)]
+    public string? Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Mật khẩu xác nhận không khớp.")]
+    public string? ConfirmPassword { get; set; }
+
+    [DisplayName("Ngày sinh")]
+    public DateTime? Dob { get; set; }
+    //ADDRESS
+
+    [DisplayName("Tên quận/huyện")]
+    public string? DistrictName { get; set; }
+
+    [DisplayName("Tên phường")]
+    public string? WardName { get; set; }
+}
---
+++ AnVatCanThoWeb/Areas/SnackBar/ViewModels/Authentication/CreateAccountViewModelValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+
+namespace AnVatCanThoWeb.Areas.SnackBar.ViewModels.Authentication;
+
+public class CreateAccountViewModelValidator : AbstractValidator<CreateAccountViewModel>
+{
+    public CreateAccountViewModelValidator()
+    {
+        RuleFor(x => x.Dob).NotEmpty()
+            .WithMessage("Vui lòng chọn ngày sinh");
+        RuleFor(x => x.Email).NotEmpty()
+            .WithMessage("Vui lòng nhập email");
+        RuleFor(x => x.Username).NotEmpty()
+            .WithMessage("Vui lòng nhập Họ và Tên");
+        RuleFor(x => x.Password).NotEmpty()
+            .WithMessage("Vui lòng nhập mật khẩu");
+        RuleFor(x => x.Tel).NotEmpty()
+            .WithMessage("Vui lòng nhập số điện thoại");
+        RuleFor(x => x.Tel).MaximumLength(10)
+            .MinimumLength(10)
+            .WithMessage("Số điện thoại có 10 chữ số");
+        RuleFor(x => x.ConfirmPassword).NotEmpty()
+            .WithMessage("Vui lòng nhập lại mật khẩu");
+        RuleFor(x => x.Dob)
+            .Must(BeAValidDateOfBirth)
+            .WithMessage("Ngày sinh phải nhỏ hơn ngày hiện tại");
+    }
+
+    private bool BeAValidDateOfBirth(DateTime? dateOfBirth)
+    {
+        return dateOfBirth <= DateTime.Now;
+    }
+}
